f3a60fcc0f7fc863779bd1df573a93bbb7734318
Cody Chapman <cody.chapman@gmail.com>
2026-04-20 13:01:41 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# set -e
-
 set -euo pipefail
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
-
-
-declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /etc/container.env
-chmod 0644 /etc/container.env
 
 # Start cron in the background and tail logs to keep container alive
 printenv > /etc/environment # Ensure cron sees env variables
