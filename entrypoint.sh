81b392acf9113df2b61952fa7db19e1029bacdda
Cody Chapman <cody.chapman@gmail.com>
2026-04-21 04:00:19 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
 echo $TZ > /etc/timezone
 
+# creating logging files before user
+touch /var/log/opensmtpd/opensmtpd.log
+touch /var/log/opensmtpd/opensmtpd.err.log
+
 # Start cron in the background and tail logs to keep container alive
 printenv > /etc/environment # Ensure cron sees env variables
 echo "Launching supervisor..."
